a59dcc41b7e02f80c4f35fcac3f197adf89ccde3
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-11-27 04:53:45 +0300

    Create style.tsx

---
+++ front-end/management/src/app/style/style.tsx
@@ -0,0 +1,87 @@
+import localFont from "next/font/local";
+
+
+const Nacelle = {
+    Regular: localFont({
+        src: "./fonts/Nacelle/Nacelle-Regular.otf",
+        variable:"--font-Nacelle-Regular",
+        weight:"100 900",
+    }),  
+    Black: localFont({
+        src: "./fonts/Nacelle/Nacelle-Black.otf",
+        variable:"--font-Nacelle-Black",
+        weight:"100 900",
+    }),  
+    BlackItalic: localFont({
+        src: "./fonts/Nacelle/Nacelle-BlackItalic.otf",
+        variable:"--font-Nacelle-BlackItalic",
+        weight:"100 900",
+    }),  
+    Bold: localFont({
+        src: "./fonts/Nacelle/Nacelle-Bold.otf",
+        variable:"--font-Nacelle-Bold",
+        weight:"100 900",
+    }),  
+    BoldItalic: localFont({
+        src: "./fonts/Nacelle/Nacelle-BoldItalic.otf",
+        variable:"--font-Nacelle-BoldItalic",
+        weight:"100 900",
+    }),  
+    Heavy: localFont({
+        src: "./fonts/Nacelle/Nacelle-Heavy.otf",
+        variable:"--font-Nacelle-Heavy",
+        weight:"100 900",
+    }),  
+    HeavyItalic: localFont({
+        src: "./fonts/Nacelle/Nacelle-HeavyItalic.otf",
+        variable:"--font-Nacelle-HeavyItalic",
+        weight:"100 900",
+    }),
+    Italic: localFont({
+        src: "./fonts/Nacelle/Nacelle-Italic.otf",
+        variable:"--font-Nacelle-Italic",
+        weight:"100 900",
+    }),  
+    Light: localFont({
+        src: "./fonts/Nacelle/Nacelle-Light.otf",
+        variable:"--font-Nacelle-Light",
+        weight:"100 900",
+    }),  
+    LightItalic: localFont({
+        src: "./fonts/Nacelle/Nacelle-LightItalic.otf",
+        variable:"--font-Nacelle-LightItalic",
+        weight:"100 900",
+    }),  
+    SemiBold: localFont({
+        src: "./fonts/Nacelle/Nacelle-SemiBold.otf",
+        variable:"--font-Nacelle-SemiBold",
+        weight:"100 900",
+    }),  
+    SemiBoldItalic: localFont({
+        src: "./fonts/Nacelle/Nacelle-SemiBoldItalic.otf",
+        variable:"--font-Nacelle-SemiBoldItalic",
+        weight:"100 900",
+    }),  
+    Thin: localFont({
+        src: "./fonts/Nacelle/Nacelle-Thin.otf",
+        variable:"--font-Nacelle-Thin",
+        weight:"100 900",
+    }),    
+    ThinItalic: localFont({
+        src: "./fonts/Nacelle/Nacelle-ThinItalic.otf",
+        variable:"--font-Nacelle-ThinItalic",
+        weight:"100 900",
+    }),  
+    UltraLight: localFont({
+        src: "./fonts/Nacelle/Nacelle-UltraLight.otf",
+        variable:"--font-Nacelle-UltraLight",
+        weight:"100 900",
+    }),  
+    UltraLightItalic: localFont({
+        src: "./fonts/Nacelle/Nacelle-UltraLightItalic.otf",
+        variable:"--font-Nacelle-UltraLightItalic",
+        weight:"100 900",
+    }),
+};
+
+export default Nacelle;
